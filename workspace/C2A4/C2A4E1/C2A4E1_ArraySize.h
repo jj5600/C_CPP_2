f628bb13afcd95831ea7768ce2e100edbf5bceac
jj5600 <57789684+jj5600@users.noreply.github.com>
2025-08-27 13:38:39 -0700

    Create C2A4E1_ArraySize.h

---
+++ workspace/C2A4/C2A4E1/C2A4E1_ArraySize.h
@@ -0,0 +1,14 @@
+//
+//  C2A4E1_ArraySize.h
+//  CPP_JD
+#ifndef C2A4E1_ArraySize_h
+#define C2A4E1_ArraySize_h
+// specify each of the 4 dimensions as explained in the problem
+constexpr int D0 = 10;
+constexpr int D1 = 7;
+constexpr int D2 = 6;
+constexpr int D3 = 8;
+// calculate TOTAL_ELEM 
+constexpr int TOTAL_ELEM = D0 * D1 * D2 * D3;
+
+#endif /* C2A4E1_ArraySize_h */
